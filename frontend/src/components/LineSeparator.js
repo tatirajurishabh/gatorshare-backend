46b3daa2f12935d8f8e63e12f1601fbbd2ffd4b0
valasaladinesh <valasaladinesh@gmail.com>
2022-02-02 11:44:15 -0500

    created basic navbar for the dashboard

---
+++ frontend/src/components/LineSeparator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LineSeparator = () => {
+  return (
+    <div className="d-flex text-center justify-content-center" style={{/*height: "20px",*/ alignItems: 'center', justifyContent: 'center'}}>
+        <div className="vr"></div>
+    </div>
+  );
+}
+
+
+export default LineSeparator
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,5 +1,49 @@
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import React from 'react'
+import LineSeparator from './LineSeparator';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
+
+import "./NavBar.css";
+
 const NavBar = () => {
-    return null
+    return (
+        <Router>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand href="#home">GatorShare</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">Profile</Nav.Link>
+                            <LineSeparator/>
+                            <Nav.Link href="#deets">Your posts</Nav.Link>
+                            <LineSeparator/>
+                            <Nav.Link href="#deets">Create</Nav.Link>
+                            <LineSeparator/>
+                            <Nav.Link eventKey={2} href="#memes">
+                                Notifications
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            {/* <Routes>
+                <Route path="/" element={<Things thingsList={things} />} />
+                <Route exact path="/services" element={<Services lstThings={things} />} />
+                <Route exact path="/relations" element={<Relationships servicesList={things} />} />
+                <Route exact path="/recipe" element={<Recipe servicesList={things} />} />
+                <Route exact path="/apps" element={<Apps servicesList={things} />} />
+            </Routes> */}
+        </Router>
+    )
 }
 
 export default NavBar
