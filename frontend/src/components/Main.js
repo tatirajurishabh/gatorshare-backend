b74a3cc3d8c3881670d1d2eed5760a1d4205a1a0
Rishabh Tatiraju <me@rishabh.blog>
2022-02-03 21:18:50 -0500

    Fix unused dependency error in build

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Col, Container, Row } from "react-bootstrap"
+import { Col, Row } from "react-bootstrap"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "../pages/Home"
 import NavBar from "./NavBar"
